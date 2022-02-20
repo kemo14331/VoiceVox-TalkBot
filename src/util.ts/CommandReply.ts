ec17eb74cabd9705bdef1e79f073979ee4d8ec3c
kemo14331 <kemo14331@gmail.com>
2022-02-20 11:43:03 +0900

    ⚡️ Replyに絵文字の追加

--- src/util.ts/CommandReply.ts
+++ src/util.ts/CommandReply.ts
@@ -3,14 +3,14 @@ import { InteractionReplyOptions } from 'discord.js';
 export class CommandReply {
     static error(content: string, ephemeral: boolean = false): InteractionReplyOptions {
         return {
-            embeds: [{ color: 'RED', description: content }],
+            embeds: [{ color: 'RED', title: '❌ Error', description: content }],
             ephemeral: ephemeral,
         };
     }
 
     static info(content: string, ephemeral: boolean = false): InteractionReplyOptions {
         return {
-            embeds: [{ color: 'AQUA', description: content }],
+            embeds: [{ color: 'AQUA', title: '✅ Info', description: content }],
             ephemeral: ephemeral,
         };
     }
